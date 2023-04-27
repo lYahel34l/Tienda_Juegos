a6860dd391d168f6323178f4e276b571bf186fec
Yahel34 <luevanogarciaorlando@gmail.com>
2023-04-27 11:23:43 -0600

    Primer Commit de Juguetes

---
+++ database/migrations/2023_04_26_205229_create_proveedor_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proveedor', function (Blueprint $table) {
+            $table->id('proveedor_id');
+            $table->string('nombre_prov');
+            $table->string('contacto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proveedor');
+    }
+};
